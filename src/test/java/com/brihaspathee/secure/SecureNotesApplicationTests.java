cbde1ca39e3c551f1d579ff79c885c4b17059ced
vbalaji215-dev <vbalaji215@outlook.com>
2025-01-12 07:42:42 -0500

    Initial add

---
+++ src/test/java/com/brihaspathee/secure/SecureNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brihaspathee.secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
