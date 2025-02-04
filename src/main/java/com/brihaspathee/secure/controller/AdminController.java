62b996f5679c1418120757e71edd56f3dcc43c9d
vbalaji215-dev <vbalaji215@outlook.com>
2025-02-04 09:49:51 -0500

    Added admin controller

--- src/main/java/com/brihaspathee/secure/controller/AdminController.java
+++ src/main/java/com/brihaspathee/secure/controller/AdminController.java
@@ -6,6 +6,7 @@ import com.brihaspathee.secure.service.interfaces.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,6 +28,7 @@ public class AdminController {
 
     private final UserService userService;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @GetMapping("/getusers")
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
